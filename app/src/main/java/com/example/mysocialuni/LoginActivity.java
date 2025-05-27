d654ccc87eb8fbd8cf02ced94ec74755b1633a3f
BeratSerce <153725218+BeratSerce@users.noreply.github.com>
2025-05-27 15:02:00 +0300

    Giriş Sayfası Eklendi

--- app/src/main/java/com/example/mysocialuni/LoginActivity.java
+++ app/src/main/java/com/example/mysocialuni/LoginActivity.java
@@ -62,6 +62,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
+
                 finish();
             }
         });
